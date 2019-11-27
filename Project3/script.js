61aa47955cc9cce7af48602023c14d431943f1f8
Zcyyyyyy <chenyu@cca.edu>
2019-11-26 16:04:57 -0800

    p3

---
+++ Project3/script.js
@@ -0,0 +1,12 @@
+
+let c_30 = document.querySelector('#c_30')
+
+c_30.onclick = function () {
+    c_30.classList.toggle('active')
+}
+
+let c = document.querySelector('#c')
+
+c.onclick = function () {
+    c.classList.toggle('active')
+}
