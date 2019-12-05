d5d12c3ea3131a5359577b0ed67379055a331c52
Zcyyyyyy <chenyu@cca.edu>
2019-12-05 15:51:07 -0800

    final

--- Project3/script.js
+++ Project3/script.js
@@ -10,3 +10,29 @@ let c = document.querySelector('#c')
 c.onclick = function () {
     c.classList.toggle('active')
 }
+
+let circles = document.querySelectorAll('.circle')
+
+for (let counter = 0; counter < circles.length; counter++) {
+
+    if (counter == circles.length - 1) {
+        circles[counter].addEventListener('mousedown', function () {
+            if (circles[counter].parentNode.classList.contains('mask')) {
+                circles[counter].parentNode.classList.toggle('active')
+            }
+        })
+    } else {
+        circles[counter].addEventListener('mousedown', function () {
+            if (circles[counter].parentNode.classList.contains('mask')) {
+                circles[counter].parentNode.classList.add('active')
+            }
+        })
+        circles[counter].addEventListener('mouseup', function () {
+            if (circles[counter].parentNode.classList.contains('mask')) {
+                circles[counter].parentNode.classList.remove('active')
+            }
+        })
+    }
+
+}
+
